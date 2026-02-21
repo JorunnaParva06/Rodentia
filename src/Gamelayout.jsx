ddfc48a4c857c6f554d95a6a2763b1dd6c89e178
JorunnaParva06 <jedwua9@gmail.com>
2026-02-20 17:27:18 -0700

    Textbox on side for card info

--- src/Gamelayout.jsx
+++ src/Gamelayout.jsx
@@ -1,9 +1,11 @@
 import Handlayout from "./Handlayout";
 import Boardlayout from "./Boardlayout";
+import Informationlayout from "./Informationlayout";
 
 function Gamelayout() {
     return (
         <div>
+            <Informationlayout></Informationlayout>
             <Boardlayout></Boardlayout>
             <Handlayout></Handlayout>
         </div>
